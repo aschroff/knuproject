f344c1e594b16ee774454536aa34d67732d9d472
andreasschroff <andreas@schroff-online.com>
2022-11-04 16:46:59 +0100

    Increased model

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knuproject.knuproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knuproject.settings')
     print('hallo')
     try:
         from django.core.management import execute_from_command_line
